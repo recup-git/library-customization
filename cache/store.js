ebdcac9d10b92ee7dca243ca72b67f3df90693ad
Carlo Zottmann <carlo@zottmann.org>
2021-07-21 18:34:32 +0200

    [CHG] Adjusts redis key prefix

--- cache/store.js
+++ cache/store.js
@@ -1,25 +1,27 @@
-'use strict'
+"use strict";
 
 // this is a sample cache integration using redis instead of the default in-memory cache
-const cacheManager = require('cache-manager')
-const redisStore = require('cache-manager-ioredis')
-const log = require('../../server/logger')
+const cacheManager = require("cache-manager");
+const redisStore = require("cache-manager-ioredis");
+const log = require("../../server/logger");
 
 // If redis URI found, use redis, otherwise fall back to in memory cache
-const cache = process.env.REDIS_URI ? cacheManager.caching({
-  store: redisStore,
-  host: process.env.REDIS_URI,
-  password: process.env.REDIS_PASS,
-  port: 6379,
-  keyPrefix: 'your-prefix-here',
-  db: 0,
-  ttl: 0 // default ttl is infinite
-}) : cacheManager.caching({ store: 'memory' })
+const cache = process.env.REDIS_URI
+  ? cacheManager.caching({
+      store: redisStore,
+      host: process.env.REDIS_URI,
+      password: process.env.REDIS_PASS,
+      port: 6379,
+      keyPrefix: "library",
+      db: 0,
+      ttl: 0, // default ttl is infinite
+    })
+  : cacheManager.caching({ store: "memory" });
 
-  // if we are using a redis instance, listen for errors
+// if we are using a redis instance, listen for errors
 if (process.env.REDIS_URI) {
-  const redisClient = cache.store.getClient()
-  redisClient.on('error', (err) => log.error('ERROR FROM REDIS CLIENT:', err))
+  const redisClient = cache.store.getClient();
+  redisClient.on("error", (err) => log.error("ERROR FROM REDIS CLIENT:", err));
 }
 
-module.exports = cache
+module.exports = cache;
